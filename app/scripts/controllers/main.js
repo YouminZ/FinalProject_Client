af9dfd0bccd2e2e0d9a7a2510aa86c09831c25f1
Youmin Zheng <zheng.youmin@gmail.com>
2016-03-23 17:00:33 -0400

    Initialized the FinalProject-Client

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testProjectsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testProjectsApp
+ */
+angular.module('testProjectsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
