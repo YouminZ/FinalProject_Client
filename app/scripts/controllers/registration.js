6903b4fe4798d6294153917a1ca365bc0df29e91
Steven Nguyen <snguyen@oakland.edu>
2016-03-30 22:37:21 -0400

    fixed issue with collapse navbar and dropdown not working

--- app/scripts/controllers/registration.js
+++ app/scripts/controllers/registration.js
@@ -8,6 +8,24 @@
  * Controller of the testProjectsApp
  */
 angular.module('testProjectsApp')
-  .controller('RegistrationCtrl', function () {
+  .controller('RegistrationCtrl', function ($scope, $http, $log, $parse, $location) {
     $('#login-modal').modal('hide');
+
+    $scope.register = function(){
+      $scope.registerForm.$setPristine(true);
+      console.log($scope.user);
+      $http.post('api/register', $scope.user)
+        // Display error when server returns error
+        .error(function(data, status){
+          if(status === 400)
+          {
+            $scope.err = data;
+            console.log(data);
+          }
+        })
+        // Do something when successful
+        .success(function(data, status){
+//          $location.path("/home")
+        })
+    }
   });
