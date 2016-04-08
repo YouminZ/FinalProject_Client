5a02caa8e7c456aed3d63ddc9237a9f5fe770943
Erik Hoggard <erikhoggard@gmail.com>
2016-04-08 18:39:46 -0400

    Added gamerTag search functionality

--- app/scripts/controllers/playersearch.js
+++ app/scripts/controllers/playersearch.js
@@ -9,6 +9,27 @@
  */
 angular.module('clientApp')
   .controller('PlayersearchCtrl', function ($scope, $http, $location) {
+    //$scope.players = [
+    //  {gamerTag: "gamertag"}
+    //];
+
+    $scope.search = function () {
+      $scope.players = [];
+      $http.get('/api/searchPlayers?search_string=' +
+        $scope.search_string, null)
+        .success(searchSuccess)
+        .error(searchFailure);
+    }
+
+      var searchSuccess = function (data, status) {
+          $scope.players = data;
+      }
+
+      var searchFailure = function(data, status) {
+      };
+
+
+
     $(document).scrollTop(0);
     $scope.gotoSearchTournament = function(){
       $location.path("/searchtournaments")
