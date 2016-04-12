3df527ffc8908b20e1c7ae4ede00787cfcb54259
Erik Hoggard <erikhoggard@gmail.com>
2016-04-12 19:15:59 -0400

    added dynamic user profiles, updated player search results to link to them

--- app/scripts/controllers/editprofile.js
+++ app/scripts/controllers/editprofile.js
@@ -8,10 +8,43 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('EditprofileCtrl', function ($scope, $http, $location) {
+  .controller('EditprofileCtrl', function ($scope, $http, $location, $cookieStore) {
     $(document).scrollTop(0);
 
+    $scope.user = $cookieStore.get('user');
+    //$scope.user = null;
+
+
     $scope.cancelUpdate = function(){
-      $location.path("/playerprofile");
+      //$scope.user = $cookieStore.get('user');
+      $location.path("/playerprofile" + $scope.user.gamerTag);
     }
+
+    $scope.editProfile = function() {
+      //$scope.user = $cookieStore.get('user');
+      $http.post('api/editProfile', $scope.user)
+      .error(function(data, status) {
+        if(status === 400)
+        {
+          $scope.err = data;
+          console.log($scope.err);
+        }
+      })
+        .success(function(data, status) {
+          $location.path("/playerprofile/" + $scope.user.gamerTag);
+        })
+    }
+
+
+//THIS BREAKS EVERYTHING
+//    var userId = $scope.user.id;
+//    if(userId !== 0) {
+//      $http.get('api/getProfile?search_string=' + $scope.user.gamerTag, null)
+//        .success(function (data, status) {
+//          $scope.user = data;
+//        })
+//        .error(function (data, status) {
+//          // Do nothing
+//        });
+//    }
   });
